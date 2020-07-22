7bc07bd43fb9495bdcdab0176e90555a0a3ca3c7
Adnan Reza <adnan.reza@outlook.com>
2020-07-22 16:13:11 -0700

    Added axios GET request in useEffect hook

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,10 +1,13 @@
 import { useReducer, useEffect } from 'react';
+import axios from 'axios';
 
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
   ERROR: 'error',
 };
+
+const BASE_URL = 'https://jobs.github.com/positions.json';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -29,11 +32,17 @@ export default function useFetchJobs(params, page) {
 
   useEffect(() => {
     dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
   }, [params, page]);
 
-  return {
-    jobs: [],
-    loading: false,
-    error: true,
-  };
+  return state;
 }
