5b8d778a5b2d22e598132f08234ae26cf27b3f4d
Adnan Reza <adnan.reza@outlook.com>
2020-07-22 16:25:50 -0700

    Created Job component to fetch jobs and display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
   const { jobs, loading, error } = useFetchJobs();
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error, try refreshing</h1>}
-      <h1>{jobs.length}</h1>
+      <h1>
+        {jobs.map((job) => {
+          return <Job key={job.id} job={job} />;
+        })}
+      </h1>
     </Container>
   );
 }
