c65d9f2f7c9c544c30c0d315c4d10ac97094c522
Adnan Reza <adnan.reza@outlook.com>
2020-07-22 16:47:07 -0700

    Added styling and info for Job component

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Card, Badge } from 'react-bootstrap';
 
 export default function Job({ job }) {
-  return <div>{job.title}</div>;
+  return (
+    <Card>
+      <Card.Body>
+        <div className='d-flex justify-content-between'>
+          <div>
+            <Card.Title>
+              {job.title} -{' '}
+              <span className='text-muted font-weight-light'>
+                {job.company}
+              </span>
+            </Card.Title>
+            <Card.Subtitle className='text-muted mb-2'>
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant='secondary' className='mr-2'>
+              {job.type}
+            </Badge>
+            <Badge variant='secondary' className='mr-2'>
+              {job.location}
+            </Badge>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
